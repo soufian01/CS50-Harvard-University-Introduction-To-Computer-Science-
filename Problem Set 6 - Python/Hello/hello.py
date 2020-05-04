f7b1332bd2abf8f4c86ff91e10e658afd86f7933
Soufian Loukili <36426403+soufian01@users.noreply.github.com>
2020-05-04 11:09:54 +0200

    Update hello.py

--- Problem Set 6 - Python/Hello/hello.py
+++ Problem Set 6 - Python/Hello/hello.py
@@ -1,4 +1,2 @@
-from cs50 import get_string # Importing the get_string function from the cs50 library.
-
-name = get_string("What is you name?\n") # Getting the user name using the get_string function.
-print("hello, " + name) # Printing hello followed by the name who was given to the program by the user.
+name = input("What is you name?\n")
+print(f"hello, {name}")
