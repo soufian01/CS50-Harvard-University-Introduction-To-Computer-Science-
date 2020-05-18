596a619cf1384cfd8c6b69e5ae677c61a4899b61
Soufian Loukili <36426403+soufian01@users.noreply.github.com>
2020-05-18 15:44:04 +0200

    Update 11.sql

--- Problem Set 7 - SQL/movies/11.sql
+++ Problem Set 7 - SQL/movies/11.sql
@@ -1,1 +1,7 @@
-a
+SELECT title FROM movies 
+JOIN people, ratings, stars 
+ON people.id = stars.person_id 
+AND stars.movie_id = ratings.movie_id 
+AND ratings.movie_id = movies.id 
+WHERE name = "Chadwick Boseman" 
+ORDER BY rating DESC LIMIT 5;
