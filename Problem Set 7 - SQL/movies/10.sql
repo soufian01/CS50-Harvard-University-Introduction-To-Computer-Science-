4420b5ddf1e66a32fd775adb28b144b0951fda6d
Soufian Loukili <36426403+soufian01@users.noreply.github.com>
2020-05-18 15:43:29 +0200

    Update 10.sql

--- Problem Set 7 - SQL/movies/10.sql
+++ Problem Set 7 - SQL/movies/10.sql
@@ -1,1 +1,6 @@
-a
+SELECT DISTINCT name FROM people
+JOIN ratings, directors, movies
+ON people.id = directors.person_id
+AND  movies.id = ratings.movie_id
+AND movies.id = ratings.movie_id
+WHERE rating >= 9.0;
