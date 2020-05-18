aa645a400fe33719889c36e2bd8ea5de9791396d
Soufian Loukili <36426403+soufian01@users.noreply.github.com>
2020-05-18 15:44:22 +0200

    Update 12.sql

--- Problem Set 7 - SQL/movies/12.sql
+++ Problem Set 7 - SQL/movies/12.sql
@@ -1,1 +1,6 @@
-1
+SELECT title FROM movies JOIN stars, people
+ON movies.id = stars.movie_id
+AND stars.person_id = people.id
+name = "Johnny Depp" AND movies.title
+IN (SELECT movies.title FROM people JOIN stars ON people.id = stars.person_id
+JOIN movies ON stars.movie_id = movies.id WHERE name = "Helena Bonham Carter");
